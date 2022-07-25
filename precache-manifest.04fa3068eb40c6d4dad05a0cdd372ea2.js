ad408a7b93b2c7979887c56972998555a2f8a4de
Md Shoaib Akhtar <mdshoaibakhtar966@gmail.com>
2022-07-25 11:39:01 +0530

    Updates

---
+++ precache-manifest.04fa3068eb40c6d4dad05a0cdd372ea2.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "430a57ac11cec96b718acbe17be5294a",
+    "url": "/powergym/index.html"
+  },
+  {
+    "revision": "248223251b42a0ac5e11",
+    "url": "/powergym/static/css/main.057a26d4.chunk.css"
+  },
+  {
+    "revision": "5e00e90f40d452c78cc9",
+    "url": "/powergym/static/js/2.58726b04.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/powergym/static/js/2.58726b04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "248223251b42a0ac5e11",
+    "url": "/powergym/static/js/main.bd7ac9a7.chunk.js"
+  },
+  {
+    "revision": "520a93e0e78a85e268a5",
+    "url": "/powergym/static/js/runtime-main.680ccc65.js"
+  },
+  {
+    "revision": "ae2508775e7b40b7f9678a27610e3578",
+    "url": "/powergym/static/media/bgVideo.ae250877.mp4"
+  },
+  {
+    "revision": "dd1df8f478bb28433b5dfc6df4832f09",
+    "url": "/powergym/static/media/classFour.dd1df8f4.jpg"
+  },
+  {
+    "revision": "5a6c841c01f957d21afe705bf1754ce0",
+    "url": "/powergym/static/media/classOne.5a6c841c.jpg"
+  },
+  {
+    "revision": "3d605f688128d539f1898c2c8867814b",
+    "url": "/powergym/static/media/classThree.3d605f68.jpg"
+  },
+  {
+    "revision": "a6ebee97d6a29927f91b2a238d1b8336",
+    "url": "/powergym/static/media/classTwo.a6ebee97.jpg"
+  },
+  {
+    "revision": "31cb05f3e1c4697812b2104049d21c55",
+    "url": "/powergym/static/media/contactBg.31cb05f3.jpg"
+  },
+  {
+    "revision": "f042c2ac7fe16211f6c2806e04fdebad",
+    "url": "/powergym/static/media/gymOverlayBg.f042c2ac.jpg"
+  },
+  {
+    "revision": "02e364823438fedfc552123239968bf6",
+    "url": "/powergym/static/media/scheduleBg.02e36482.jpg"
+  },
+  {
+    "revision": "7c2ab25d46f7a6f96ce234d934f56a80",
+    "url": "/powergym/static/media/trainerOne.7c2ab25d.jpg"
+  },
+  {
+    "revision": "81e56a6dd10134e1affed0e5f247541f",
+    "url": "/powergym/static/media/trainerThree.81e56a6d.jpg"
+  },
+  {
+    "revision": "db6c16448e4272b3e4eb88cdc84c356f",
+    "url": "/powergym/static/media/trainerTwo.db6c1644.jpg"
+  }
+]);
